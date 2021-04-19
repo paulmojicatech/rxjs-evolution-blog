2fe8d6f112fae6679763cb4c30196de72c87010a
paulmojicatech <paulmojicatech@gmail.com>
2021-04-19 15:49:42 -0400

    more like logic

--- src/app/subscribes/components/subscribes.component.ts
+++ src/app/subscribes/components/subscribes.component.ts
@@ -71,7 +71,7 @@ export class SubscribesComponent implements OnInit, OnDestroy {
             if (sectionPlayer.name === player.name) {
               return {
                 ...player,
-                likes: player.likes + 1
+                likes: !!player.likes ? player.likes + 1 : 1
               };
             };
           });
