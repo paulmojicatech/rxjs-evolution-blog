1536c95973a4497416af2a6c9dc8a3f609a860c6
paulmojicatech <paulmojicatech@gmail.com>
2021-04-20 01:12:08 -0400

    add side panel content

--- src/shared-components/side-panel/components/side-panel.component.ts
+++ src/shared-components/side-panel/components/side-panel.component.ts
@@ -4,6 +4,7 @@ import { IPlayerOverview } from '../../../app/models/players.interface';
 @Component({
     selector: 'app-side-panel',
     templateUrl: './side-panel.component.html',
+    styleUrls: ['./side-panel.component.scss']
 })
 export class SidePanelComponent {
     @Input()
