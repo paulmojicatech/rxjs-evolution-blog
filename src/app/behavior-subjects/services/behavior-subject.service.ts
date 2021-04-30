1085ae40e1f9046fe59c8b011bd88ef6373f150c
paulmojicatech <paulmojicatech@gmail.com>
2021-04-30 08:07:13 -0400

    behavior subject service wip

--- src/app/behavior-subjects/services/behavior-subject.service.ts
+++ src/app/behavior-subjects/services/behavior-subject.service.ts
@@ -1,6 +1,15 @@
 import { Injectable } from "@angular/core";
+import { Observable, Subject } from "rxjs";
+import { IBehaviorSubjectViewModel } from "../../models/players.interface";
 
 @Injectable()
 export class BehaviorSubjectService {
-  private _viewModelSub$ = new Subject<I
+  
+  private _viewModelSub$ = new Subject<IBehaviorSubjectViewModel>();
+  viewModel$ = this._viewModelSub$.asObservable();
+
+  getViewModel(): Observable<IBehaviorSubjectViewModel> {
+    return this.viewModel$;
+  }
+
 }
