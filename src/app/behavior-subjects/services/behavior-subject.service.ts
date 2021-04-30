d27a121371a90d9de107bde7907abd68ffdbbc18
paulmojicatech <paulmojicatech@gmail.com>
2021-04-30 08:20:08 -0400

    continue work on behavior subject state service

--- src/app/behavior-subjects/services/behavior-subject.service.ts
+++ src/app/behavior-subjects/services/behavior-subject.service.ts
@@ -1,15 +1,51 @@
 import { Injectable } from "@angular/core";
 import { Observable, Subject } from "rxjs";
-import { IBehaviorSubjectViewModel } from "../../models/players.interface";
+import { IBehaviorSubjectViewModel, IPlayerOverview, IPositionSections, PlayerPositionType } from "../../models/players.interface";
+import { PlayerHttpService } from "../../services/player-http.service";
 
 @Injectable()
 export class BehaviorSubjectService {
-  
+
   private _viewModelSub$ = new Subject<IBehaviorSubjectViewModel>();
   viewModel$ = this._viewModelSub$.asObservable();
 
+  constructor(private _playerHttpSvc: PlayerHttpService){ }
+
   getViewModel(): Observable<IBehaviorSubjectViewModel> {
     return this.viewModel$;
+  }
+
+  private setupSections(): IPositionSections[] {
+    return [
+      {
+        position: PlayerPositionType.PG,
+        players: [],
+      },
+      {
+        position: PlayerPositionType.SG,
+        players: [],
+      },
+      {
+        position: PlayerPositionType.SF,
+        players: [],
+      },
+      {
+        position: PlayerPositionType.PF,
+        players: [],
+      },
+      {
+        position: PlayerPositionType.C,
+        players: [],
+      },
+    ];
+  }
+
+  private getPlayers(): Observable<IPlayerOverview[]> {
+    return this._playerHttpSvc.getPlayers();
+  }
+
+  private addPlayersToSections(positionSections: IPositionSections[], player: IPlayerOverview): IPositionSections[] {
+    return null;
   }
 
 }
