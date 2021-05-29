c09f32452d8296a06f5a25fae664b6b4bb4cd9fe
paulmojicatech <paulmojicatech@gmail.com>
2021-05-29 09:13:32 -0400

    1 file modified

--- src/app/behavior-subjects/behavior-subjects.module.ts
+++ src/app/behavior-subjects/behavior-subjects.module.ts
@@ -1,12 +1,24 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from "@angular/material/button";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatListModule } from "@angular/material/list";
+import { MatSidenavModule } from "@angular/material/sidenav";
 import { BehaviorSubjectComponent } from "./components/behavior-subject.component";
 
 @NgModule({
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatExpansionModule,
+    MatInputModule,
+    MatListModule,
+    MatSidenavModule
   ],
   declarations: [
     BehaviorSubjectComponent
